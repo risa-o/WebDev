7cd58dc3a8fcd6112b166ca8dbebf215c79de70c
risa-o <risa.orsinger@gmail.com>
2019-04-16 09:20:52 -0500

    looping through an array

--- js/iterating.js
+++ js/iterating.js
@@ -2,7 +2,7 @@
     "use strict";
 
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
-    var names = ['James','Shannon','Thao','Risa'];
+    var names = ['Shannon','James','Thao','Risa'];
     // TODO: Create a log statement that will log the number of elements in the names array.
     console.log(names.length);
     // TODO: Create log statements that will print each of the names array elements individually.
@@ -10,4 +10,14 @@
     console.log(names[1]);
     console.log(names[2]);
     console.log(names[3]);
+
+    console.log(names[0] + '\n' + names [1] + '\n' + names[2] + '\n' + names[3]);
+
+    for (var i = 0; i < names.length; i++){
+    	console.log(names[i]);
+    };
+
+    names.forEach(function(element,index, array){
+    	console.log(element)
+    });
 })();
